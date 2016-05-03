7a8537383c971c7ee60a96a612a63e2aabbc5314
ahrameev <striplool@gmail.com>
2016-05-03 17:28:16 +1000

    Add methods crud for record service

--- src/Monastyryov/PhoneBookBundle/Service/RecordService.php
+++ src/Monastyryov/PhoneBookBundle/Service/RecordService.php
@@ -2,19 +2,116 @@
 
 namespace Monastyryov\PhoneBookBundle\Service;
 
+use Monastyryov\PhoneBookBundle\Entity\Record;
+use Monastyryov\PhoneBookBundle\Exception\IdentificationNotFoundException;
+use Monastyryov\PhoneBookBundle\Exception\RecordNotFoundException;
+use Monastyryov\PhoneBookBundle\Exception\StreetNotFoundException;
+use Monastyryov\PhoneBookBundle\Factory\RecordFactory;
 use Monastyryov\PhoneBookBundle\Repository\RecordRepository;
+use Monastyryov\PhoneBookBundle\Repository\StreetRepository;
+use Monastyryov\PhoneBookBundle\Request\RecordRequest;
+use Psr\Log\LoggerInterface;
 
 class RecordService
 {
     /** @var RecordRepository */
     protected $recordRepository;
 
+    /** @var LoggerInterface */
+    protected $logger;
+
+    /** @var StreetRepository */
+    protected $streetRepository;
+
+    /** @var RecordFactory */
+    protected $recordFactory;
+
     /**
      * RecordService constructor.
      * @param RecordRepository $recordRepository
+     * @param RecordFactory $recordFactory
+     * @param LoggerInterface $logger
+     * @internal param StreetRepository $streetRepository
      */
-    public function __construct(RecordRepository $recordRepository)
-    {
+    public function __construct(
+        RecordRepository $recordRepository,
+        RecordFactory $recordFactory,
+        LoggerInterface $logger
+    ) {
         $this->recordRepository = $recordRepository;
+        $this->logger = $logger;
+        $this->recordFactory = $recordFactory;
+    }
+
+    /**
+     * @return Record[]
+     */
+    public function getAll()
+    {
+        return $this->recordRepository->findAll();
+    }
+
+    /**
+     * @param RecordRequest $recordRequest
+     * @throws IdentificationNotFoundException
+     * @throws StreetNotFoundException
+     */
+    public function createRecordByRequest(RecordRequest $recordRequest)
+    {
+        $record = $this->recordFactory->createByRequest($recordRequest);
+        $this->recordRepository->persist($record);
+        $this->recordRepository->flush();
+    }
+
+    /**
+     * @param Record $record
+     * @throws IdentificationNotFoundException
+     */
+    public function updateRecord(Record $record)
+    {
+        $recordFound = $this->getRecordById($record->getId());
+
+        $recordFound
+            ->setBirthDatetime($record->getBirthDatetime())
+            ->setName($record->getName())
+            ->setPatronymic($record->getPatronymic())
+            ->setPhoneNumber($record->getPhoneNumber())
+            ->setStreet($record->getStreet())
+            ->setSurname($record->getSurname());
+
+        $this->recordRepository->persist($recordFound);
+        $this->recordRepository->flush();
+    }
+
+    /**
+     * @param int $id
+     */
+    public function deleteRecord($id)
+    {
+        $record = $this->getRecordById($id);
+        $this->recordRepository->remove($record);
+        $this->recordRepository->flush();
+    }
+
+    /**
+     * @param int $id
+     * @return null|Record
+     * @throws IdentificationNotFoundException
+     * @throws RecordNotFoundException
+     */
+    protected function getRecordById($id)
+    {
+        if (null === $id) {
+            $this->logger->info('Identification of record is invalid');
+            throw new IdentificationNotFoundException();
+        }
+
+        $record = $this->recordRepository->find($id);
+        if(null === $record) {
+            $this->logger->info('Record not found', ['record_id' => $id]);
+            throw new RecordNotFoundException();
+        }
+
+        return $record;
     }
 }
