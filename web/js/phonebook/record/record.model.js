e0ea1e480e411f313d78a99fcb67120b8be646aa
ahrameev <striplool@gmail.com>
2016-05-09 22:22:35 +1000

    Record sync on update custom js

--- web/js/phonebook/record/record.model.js
+++ web/js/phonebook/record/record.model.js
@@ -1,5 +1,21 @@
 var Record = Backbone.Model.extend({
     clear: function() {
         this.destroy();
+    },
+
+    sync: function (method, model, options) {
+        if (method == 'update') {
+            model.set("record_request", {
+                "name":model.get('name'),
+                "surname":model.get('surname'),
+                "patronymic":model.get('patronymic'),
+                "birth_timestamp":model.get('birth_timestamp'),
+                "phone_number":model.get('phone_number'),
+                "street_id": model.get('street_id')
+            });
+            console.log(model);
+        }
+
+        return Backbone.sync.call(this, method, model, options);
     }
 });
