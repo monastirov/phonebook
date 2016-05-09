eabef2b270cad9f279c51975a19099e90b837d6a
ahrameev <striplool@gmail.com>
2016-05-09 16:53:52 +1000

    Add edit for record view

--- web/js/phonebook/record/record.view.js
+++ web/js/phonebook/record/record.view.js
@@ -1,11 +1,16 @@
 var RecordView = Backbone.View.extend({
     tagName:  "tr",
     className: "record",
+    cityCollection: {},
     template: _.template($('script#record-template').html()),
+    locationView: null,
     events: {
-        "click .delete" : "clear"
+        "click .destroy" : "clear",
+        "dblclick .record-item-simple"  : "edit"
+        // "blur .edit" : "close"
     },
     initialize: function(options) {
+        this.cityCollection = options.cityCollection;
         this.model.on('change', this.render, this);
         this.model.on('remove', this.clearView, this);
     },
@@ -21,5 +26,25 @@ var RecordView = Backbone.View.extend({
 
     clearView: function() {
         this.$el.html('');
+    },
+
+    edit: function() {
+        this.$el.addClass("editing");
+        this.locationView = new LocationView({
+            el: '.' + this.className,
+            model: this.model,
+            cities: this.cityCollection
+        });
+        this.locationView.render();
+    },
+
+    close: function() {
+        var model = {};
+        this.$el.find('.edit').each(function(index) {
+            model[$( this ).attr("name")] = $( this ).val();
+        });
+        console.log(model);
+        this.model.save(model);
+        this.$el.removeClass("editing");
     }
 });
