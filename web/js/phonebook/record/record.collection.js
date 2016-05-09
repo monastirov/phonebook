57e0bc1a10f7f769a3c09ced3d07bba54f78b6a2
ahrameev <striplool@gmail.com>
2016-05-09 16:52:59 +1000

    Remove unnecessary methods record collection js

--- web/js/phonebook/record/record.collection.js
+++ web/js/phonebook/record/record.collection.js
@@ -1,23 +1,3 @@
 var RecordCollection = Backbone.Collection.extend({
-    model: Record,
-
-    initialize: function(options) {
-
-    },
-
-    /**
-     * получаем место по го cid
-     * @param cid
-     * @returns {Record}
-     */
-    getModelByCid: function(cid) {
-        return this.find(function (record) {
-            return record.cid  === cid
-        });
-    },
-
-    parse: function(response, options) {
-        if (response.error) return null;
-        return response;
-    }
+    model: Record
 });
